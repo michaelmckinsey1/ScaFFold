5b096983cbd13dadb2247cc71bbd54d0064cdd03
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-01-21 15:05:28 -0800

    Changes required to make distconv a dependency

--- scripts/install-matrix.sh
+++ scripts/install-matrix.sh
@@ -1,5 +1,4 @@
 ml load python/3.11.5 && python3 -m venv .venvs/scaffoldvenv-matrix && source .venvs/scaffoldvenv-matrix/bin/activate && pip install --upgrade pip
 ml cuda/12.6.0 gcc/12.1.1 mvapich2/2.3.7
 export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
-git clone git@github.com:LBANN/DistConv.git
-pip install --no-binary=mpi4py -e .[cuda] DistConv/ --prefix=.venvs/scaffoldvenv-matrix --extra-index-url https://download.pytorch.org/whl/cu126 2>&1 | tee install.log
+pip install --no-binary=mpi4py -e .[cuda] --prefix=.venvs/scaffoldvenv-matrix --extra-index-url https://download.pytorch.org/whl/cu126 2>&1 | tee install.log
--- scripts/install-tuolumne.sh
+++ scripts/install-tuolumne.sh
@@ -1,4 +1,3 @@
 ml load python/3.11.5 && python3 -m venv .venvs/scaffoldvenv-tuo && source .venvs/scaffoldvenv-tuo/bin/activate && pip install --upgrade pip
 ml load rocm/6.4.2 rccl/fast-env-slows-mpi libfabric
-git clone git@github.com:LBANN/DistConv.git
-pip install -e .[rocmwci] DistConv/ --prefix=.venvs/scaffoldvenv-tuo 2>&1 | tee install.log
+pip install -e .[rocmwci] --prefix=.venvs/scaffoldvenv-tuo 2>&1 | tee install.log
