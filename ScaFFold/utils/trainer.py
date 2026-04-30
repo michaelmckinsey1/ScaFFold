14e3bef879bfe49afaac725fdddfa5a286c9a209
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2026-04-30 14:20:49 -0700

    Update trainer.py

--- ScaFFold/utils/trainer.py
+++ ScaFFold/utils/trainer.py
@@ -201,7 +201,8 @@ class BaseTrainer:
         )
 
         # Set up gradient scaler for AMP (Automatic Mixed Precision)
-        self.use_grad_scaler = self.config.torch_amp and self.amp_dtype == torch.float16
+        # bfloat does not need grad scaler
+        self.use_grad_scaler = self.config.torch_amp and self.amp_dtype != torch.bfloat16
         self.grad_scaler = torch.amp.GradScaler("cuda", enabled=self.use_grad_scaler)
 
         # Set up loss function
