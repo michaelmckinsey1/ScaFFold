4233c673c0fc09462ed3b799c18b514a944e1b9b
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-05-14 13:22:09 -0700

    Change wheel source and use torch2.12

--- scripts/install-tuolumne-torchpypi.sh
+++ scripts/install-tuolumne-torchpypi.sh
@@ -1,5 +1,5 @@
-ml load python/3.12.2 && python3 -m venv .venvs/scaffoldvenv-tuo-pypi && source .venvs/scaffoldvenv-tuo-pypi/bin/activate && pip install --upgrade pip
+ml load python/3.11.5 && python3 -m venv .venvs/scaffoldvenv-tuo-pypi && source .venvs/scaffoldvenv-tuo-pypi/bin/activate && pip install --upgrade pip
 ml cce/21.0.0 cray-mpich/9.1.0 rocm/7.2.1 rccl/fast-env-slows-mpi
-pip install -e .[rocm] --find-links https://repo.radeon.com/rocm/manylinux/rocm-rel-7.2.1/ 2>&1 | tee install.log
+pip install -e .[rocm] --find-links https://download.pytorch.org/whl/torch/ --find-links https://download.pytorch.org/whl/triton-rocm/ 2>&1 | tee install.log
 # libmpi.so.12 does not exist => ls /opt/cray/pe/lib64/ | grep libmpi
-patchelf --replace-needed libmpi.so.12 libmpi_gnu.so.12 .venvs/scaffoldvenv-tuo-pypi/lib/python3.12/site-packages/mpi4py/MPI.mpich.cpython-312-x86_64-linux-gnu.so
+patchelf --replace-needed libmpi.so.12 libmpi_gnu.so.12 .venvs/scaffoldvenv-tuo-pypi/lib/python3.11/site-packages/mpi4py/MPI.mpich.cpython-311-x86_64-linux-gnu.so
