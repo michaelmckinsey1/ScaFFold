7d286364ef87166cb5d364b7bb160b3dcd38df97
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2026-06-25 08:33:44 -0700

    replace batchnorm with groupnorm (#79)

--- ScaFFold/unet/unet_parts.py
+++ ScaFFold/unet/unet_parts.py
@@ -26,19 +26,27 @@ _up_annotate = annotate(fmt="Up.{}")
 _outconv_annotate = annotate(fmt="OutConv.{}")
 
 
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
+def _group_norm(num_groups, num_channels):
+    if num_channels % num_groups != 0:
+        raise ValueError(
+            f"group_norm_groups={num_groups} must evenly divide num_channels={num_channels}"
+        )
+    return nn.GroupNorm(num_groups, num_channels)
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+
+class DoubleConv(nn.Module):
+    """(convolution => GroupNorm => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, group_norm_groups, mid_channels=None):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm3d(mid_channels),
+            _group_norm(group_norm_groups, mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm3d(out_channels),
+            _group_norm(group_norm_groups, out_channels),
             nn.ReLU(inplace=True),
         )
 
@@ -50,10 +58,11 @@ class DoubleConv(nn.Module):
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, group_norm_groups):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool3d(2), DoubleConv(in_channels, out_channels)
+            nn.MaxPool3d(2),
+            DoubleConv(in_channels, out_channels, group_norm_groups),
         )
 
     @_down_annotate
@@ -64,18 +73,23 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, trilinear=True):
+    def __init__(self, in_channels, out_channels, group_norm_groups, trilinear=True):
         super().__init__()
 
         # if trilinear, use the normal convolutions to reduce the number of channels
         if trilinear:
             self.up = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = DoubleConv(
+                in_channels,
+                out_channels,
+                group_norm_groups,
+                in_channels // 2,
+            )
         else:
             self.up = nn.ConvTranspose3d(
                 in_channels, in_channels // 2, kernel_size=2, stride=2
             )
-            self.conv = DoubleConv(in_channels, out_channels)
+            self.conv = DoubleConv(in_channels, out_channels, group_norm_groups)
 
     @_up_annotate
     def forward(self, x1, x2):
